8590e63fd244278580c5bf66a6c6b55901791872
LinFeng <1329686360@qq.com>
2026-09-11 10:51:58 +0800

    proxy拦截new constructor

---
+++ proxy/proxyNewConstructor.js
@@ -0,0 +1,179 @@
+// 代理器方法封装
+
+ld = {};
+ld.config = {}; // 配置
+ld.config.proxy = true; // 是否使用代理
+
+// 获取对象类型
+ld.getType = function getType(obj) {
+    return Object.prototype.toString.call(obj);
+}
+
+/**
+ *
+ * @param obj 原始对象
+ * @param objName 原始对象的名字
+ */
+ld.proxy = function proxy(obj, objName) {
+
+    // 若不使用代理则直接返回原始对象
+    if (!ld.config.proxy) {
+        return obj;
+    }
+    let handler = {
+        /**
+         *
+         * @param target 原始对象 user
+         * @param prop 属性 且只有string和symbol两种模式
+         * @param receiver 代理后的对象
+         */
+        get(target, prop, receiver) {
+            let result;
+            // target可能会报错 要用try
+            try {
+                result = Reflect.get(target, prop, receiver);
+                // 当 result 是对象时 可以用JSON.stringfy 但是这个有bug：不能输出循环引用的对象，若有循环引用，会报错
+                // 如果是对象 我们返回类型即可
+                if (result instanceof Object) { // 不能用 typeof result === "object" 因为null也是object 用instanceof 就没事
+                    let type = ld.getType(result);
+                    console.log(`{get | obj:[${objName}] -> prop:[${prop.toString()}], type:[${type}]}`);
+                    // 递归代理
+                    result = ld.proxy(result, `${objName}.${prop.toString()}`);
+                } else {
+                    console.log(`{get | obj:[${objName}] -> prop:[${prop.toString()}], return:[${result}]}`);
+                }
+
+            } catch (e) {
+                console.log(`{get error | obj:[${objName}] -> prop:[${prop.toString()}], error:[${e.message}]}`);
+            }
+            return result;
+        }, set(target, prop, value, receiver) {
+            let result;
+            try {
+                result = Reflect.set(target, prop, value, receiver);
+                if (value instanceof Object) {
+                    let type = ld.getType(value);
+                    console.log(`{set | obj:[${objName}] -> prop:[${prop.toString()}], type:[${type}]}`);
+                    // set 不需要递归代理，因为需要设置逻辑，这里不需要
+                } else {
+                    console.log(`{set | obj:[${objName}] -> prop:[${prop.toString()}], value:[${value}]}`);
+                }
+
+            } catch (e) {
+                console.log(`{set error | obj:[${objName}] -> prop:[${prop.toString()}], error:[${e.message}]}`);
+            }
+            return result;
+        }, // 当使用Object.getOwnPropertyDescriptor方式获取属性时拦截
+        getOwnPropertyDescriptor(target, prop) {
+            let result; // 返回结果是undefined或描述符对象
+            try {
+                result = Reflect.getOwnPropertyDescriptor(target, prop);
+                let type = ld.getType(result);
+                console.log(`{getOwnPropertyDescriptor | obj:[${objName}] -> prop:[${prop.toString()}], type:[${type}]}`);
+
+                // 一般不需要拦截属性描述符，在需要的时候再拦截
+                // if (typeof result !== "undefined") { // 对象没有这个属性
+                //     result = ld.proxy(result, `${objName}.${prop.toString()}.PropertyDescriptor`);
+                // }
+            } catch (e) {
+                console.log(`{getOwnPropertyDescriptor error | obj:[${objName}] -> prop:[${prop.toString()}], error:[${e.message}]}`);
+            }
+
+            return result;
+        }, defineProperty(target, prop, descriptor) {
+            let result;
+            try {
+                result = Reflect.defineProperty(target, prop, descriptor);
+                let type = ld.getType(result);
+                console.log(`{defineProperty | obj:[${objName}] -> prop:[${prop.toString()}]}`);
+            } catch (e) {
+                console.log(`{defineProperty error | obj:[${objName}] -> prop:[${prop.toString()}], error:[${e.message}]}`);
+            }
+            return result;
+        }, /**
+         *
+         * @param target 函数对象
+         * @param thisArg 调用函数的this指针
+         * @param argumentsList 类数组，函数的入参组成的一个列表
+         */
+        apply(target, thisArg, argumentsList) {
+            let result;
+            try {
+                result = Reflect.apply(target, thisArg, argumentsList);
+                let type = ld.getType(result);
+                if (result instanceof Object) {
+                    // 输出结果尽量不要输出参数，因为不知道会遇到什么奇怪的类型，导致报错，在必要的时候再输出
+                    //console.log(`{apply | function:[${objName}] -> args:[${argumentsList}], type:[${type}]}`);
+                    console.log(`{apply | function:[${objName}], type:[${type}]}`);
+                } else if (typeof result === "symbol") {
+                    // 这里若输出 argumentsList会报错，Cannot convert a Symbol value to a string
+                    // 这里result也要进行toString 否则也会报同样的错误
+                    console.log(`{apply | function:[${objName}], result:[${result.toString()}]}`);
+                } else {
+                    //console.log(`{apply | function:[${objName}] -> args:[${argumentsList}], result:[${result}]}`);
+                    console.log(`{apply | function:[${objName}], result:[${result}]}`);
+                }
+
+            } catch (e) {
+                console.log(`{apply error | function:[${objName}], error:[${e.message}]}`);
+            }
+
+            return result;
+        },
+        /**
+         *
+         * @param target 函数对象
+         * @param argArray 参数列表
+         * @param newTarget 代理对象
+         * @returns {*}
+         */
+        construct(target, argArray, newTarget) {
+            let result;
+            try {
+                result = Reflect.construct(target, argArray, newTarget);
+                let type = ld.getType(result);
+                console.log(`{construct | function:[${objName}], type:[${type}]}`);
+
+            } catch (e) {
+                console.log(`{construct error | function:[${objName}], error:[${e.message}]}`);
+            }
+
+            return result;
+        },
+    };
+    return new Proxy(obj, handler);
+}
+
+
+let symbol = Symbol(123);
+
+let user = {
+    name: "小明", info: {
+        name: "小明", age: 29
+    }, [symbol]: "symbol123"
+
+};
+
+user = ld.proxy(user, "user");
+
+console.log(Object.getOwnPropertyDescriptor(user, "name"));
+user.hobby = "游泳";
+
+function add(a, b) {
+    return a;
+}
+
+add = ld.proxy(add, "add");
+add(1, 2);
+add({name: "Tom"}, 2);
+add(Symbol(), 2);
+
+function User() {
+
+}
+
+Object.defineProperty(User.prototype, Symbol.toStringTag, {
+    value: "UserTest"
+});
+User = ld.proxy(User, "User");
+let u = new User();
