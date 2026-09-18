de24eb2843208d869331d419550d60a47348c8c5
LinFeng <1329686360@qq.com>
2026-09-18 10:56:46 +0800

    补充笔记localStorage,sessionStorage

--- sandBox/config/config.js
+++ sandBox/config/config.js
@@ -4,7 +4,7 @@ let ldvm = {
     // 功能函数相关
     toolsFunc: {},
     config: {
-        proxy: false, // 是否开启代理
+        proxy: true, // 是否开启代理
         print: true, // 是否打印日志
     },
     envFunc: {},// 具体环境实现相关
--- sandBox/tools/proxyObj.js
+++ sandBox/tools/proxyObj.js
@@ -1,11 +1,11 @@
 // 需要代理的对象
 // window = new Proxy(window, {});
-
+localStorage = ldvm.toolsFunc.proxy(localStorage, "localStorage");
+sessionStorage = ldvm.toolsFunc.proxy(sessionStorage, "sessionStorage");
 window = ldvm.toolsFunc.proxy(window, "window");
 document = ldvm.toolsFunc.proxy(document, "document");
 location = ldvm.toolsFunc.proxy(location, "location");
-localStorage = ldvm.toolsFunc.proxy(localStorage, "localStorage");
-sessionStorage = ldvm.toolsFunc.proxy(sessionStorage, "sessionStorage");
+
 
 
 // 开启代理，是为了更好地辅助补环境，但所有环境补完之后，还是不行，那可能是Proxy被检测了，这就需要关掉代理
